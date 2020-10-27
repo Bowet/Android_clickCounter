b39a6dd89405634067bee1bd7c4ca9c1d5b8b79a
Basti <73500107+Bowet@users.noreply.github.com>
2020-10-27 19:45:18 +0100

    okidoki

---
+++ Assets/scripts/TEST.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Android;
+
+public class TEST : MonoBehaviour
+{
+    public static int CountDigit;
+    public GameObject CountDisplay;
+    public int InternalCount;
+    void Update()
+    {
+        InternalCount = CountDigit;
+        CountDisplay.GetComponent<Text>().text ="" + InternalCount;
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    
+}
+}
---
+++ Assets/scripts/TouchCount.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchCount : MonoBehaviour
+{
+    public GameObject counter;
+
+    public void TapIt ()
+    {
+        TEST.CountDigit += 1;
+    }
+}
